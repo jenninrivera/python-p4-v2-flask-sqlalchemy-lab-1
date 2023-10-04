86f4775dfef921ab135dafb0c5513711754b6ffe
Jenni Rivera <jenninrivera@gmail.com>
2023-10-04 13:04:01 -0400

    Complete lab

--- server/app.py
+++ server/app.py
@@ -22,6 +22,31 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = {'id': earthquake.id,
+                'location': earthquake.location,
+                'magnitude': earthquake.magnitude,
+                'year': earthquake.year}
+        status = 200
+
+    else:
+        body = {"message" : f"Earthquake {id} not found."}
+        status = 404
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    quakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append(earthquake.to_dict())
+    body = {"count": len(quakes),
+            "quakes": quakes}
+    status = 200
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
